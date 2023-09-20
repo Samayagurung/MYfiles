ee98e518f94b44c37aea26478f90bf538e526132
Samaya Gurung <samayagurung99@gmail.com>
2023-09-20 11:35:33 +0545

    feat: added assignment

--- day22/assignments/index.js
+++ day22/assignments/index.js
@@ -1,17 +1,16 @@
 // The goal is to find the minimum number of jumps required to reach the end of the cloud array.
 
 const clouds = [0, 0, 1, 0, 0, 1, 0];
-
-function jumpingOnCloud(c){
-  let countJump = 0;
-  for(let i = 0; i < c.length - 1;){
-    if(i+2<c.length && c[i+2]===0){
-      i+=2;
-    }else{
-      i++;
-    }
-    countJump++;
+let countJumps=0;
+function countingOnJump(c){
+for ( i = 0; i < c.length - 1;){
+  if(i+2<c.length && c[i+2] === 0){
+     i+=2;
+  }else{
+     i++;
   }
-return countJump
+  countJumps++;
 }
-console.log(jumpingOnCloud(clouds));
+return countJumps;
+}
+console.log(countingOnJump(clouds))
--- day22/assignments2/index.js
+++ day22/assignments2/index.js
@@ -5,19 +5,13 @@ const data = [
   { product: "Widget", sales: 150, region: "North" },
   { product: "Gadget", sales: 120, region: "South" },
 ];
-
 let sum = 0;
-data
-  .filter((item) => {
-    return item.region === "North";
-  })
-  .forEach((item) => {
-    sum += item.sales;
-  });
-console.log(`${sum}`);
 
-const text = "This is a simple example. This is just an example of word frequency.";
-// the result should be in object: { this: 2, is: 2, a: 1, simple: 1, example: 2, just: 1, an: 1, of: 1, word: 1, frequency: 1 }
-
- 
-
+const filterdItem = data.filter((val) => {
+  return val.region === "North";
+});
+filterdItem.forEach((val) => {
+  return (sum = sum + val.sales);
+});
+console.log(filterdItem);
+console.log("Sum of region North only:",sum);
